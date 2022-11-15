cccf884cde3da89184671c7c2a119b05f4ed3b62
Tom-Erik Thorbeck <tom.thorbeck@visma.com>
2022-11-15 12:47:54 +0100

    Content metadata

--- app/content.js
+++ app/content.js
@@ -21,33 +21,40 @@ const n2m = new NotionToMarkdown({ notionClient: notion });
       updated: page.last_edited_time,
       tags: page.properties.Tags.multi_select.map((tag) => tag.name),
       title: page.properties.Name.title[0].plain_text,
+      icon: page.icon,
+      cover: page.cover,
+      slug: page.properties.Name.title[0].plain_text
+        .toLowerCase()
+        .replace(/ /g, '-')
+        .replace(/[^\w-]+/g, ''),
     };
   });
 
   // Create data folder
   if (!fs.existsSync(process.env.PATH_CONTENT)) {
     fs.mkdirSync(process.env.PATH_CONTENT);
+    console.info('Created data folder');
   }
 
   // save json of pages metadata
   fs.writeFile('public/index.json', JSON.stringify(pages), (err) => {
-    if (err) {
-      console.log(err);
-    }
+    console.info(err ? err : 'Saved index.json');
   });
 
   // convert pages
   const markdown = pages.map(async (page) => {
-    const mdblocks = await n2m.pageToMarkdown(page.id);
-    const mdString = n2m.toMarkdownString(mdblocks);
+    const blocks = await n2m.pageToMarkdown(page.id);
+    const markdownText = n2m.toMarkdownString(blocks);
 
     fs.writeFile(
       `${process.env.PATH_CONTENT}/${page.id}.md`,
-      mdString,
+      markdownText,
       (err) => {
-        if (err) {
-          console.log(err);
-        }
+        console.info(
+          err
+            ? err
+            : `Saved ${page.title} as ${page.id}.md containing ${blocks.length} blocks.`
+        );
       }
     );
   });
