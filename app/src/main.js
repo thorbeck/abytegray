0049a383d65214cde5af2c358e227687dfb9af11
Tom-Erik Thorbeck <tom.thorbeck@visma.com>
2022-11-17 15:41:26 +0100

    Routes

--- app/src/main.js
+++ app/src/main.js
@@ -4,7 +4,6 @@ import './css/core.scss';
 import './css/normalize.scss';
 
 // CUSTOM ELEMENTS
-import './elements/content/content';
 import './elements/logo/logo';
 
 // COMPONENTS
@@ -25,3 +24,36 @@ import './resources/mstile-144x144.png';
 import './resources/mstile-150x150.png';
 import './resources/mstile-310x150.png';
 import './resources/mstile-310x310.png';
+
+// ROUTES
+window.addEventListener(
+  'abgRouter',
+  (event) => {
+    // set routes
+    const routes = [
+      {
+        slug: 'article',
+        title: 'Home',
+        markup: '<h1>Home</h1>',
+      },
+      {
+        slug: 'about',
+        title: 'About',
+        markup: '<h1>About</h1>',
+      },
+    ];
+    // set props
+    event.target.home = {
+      slug: '/',
+      title: 'Home',
+      markup: '<h1>Home</h1>',
+    };
+    event.target.notFound = {
+      slug: '404',
+      title: 'Page Not Found',
+      markup: '<h1>404</h1>',
+    };
+    event.target.routes = routes;
+  },
+  { once: true }
+);
