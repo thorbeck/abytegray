c924cb8056c33c50b5acdeb5b351a529f00e45ee
Tom-Erik Thorbeck <tom.thorbeck@visma.com>
2022-11-17 15:41:12 +0100

    Hash based router.

---
+++ components/src/components/router/router.tsx
@@ -0,0 +1,46 @@
+import { Component, Event, EventEmitter, h, Host, Prop, State, Watch } from '@stencil/core';
+
+import { RouterItem } from './router.interface';
+
+@Component({
+  tag: 'abg-router',
+  shadow: false,
+})
+export class Router {
+  @Prop() home: RouterItem;
+  @Prop() notFound: RouterItem;
+  @Prop() routes: RouterItem[];
+  @State() markup: string;
+
+  @Event() abgRouter: EventEmitter;
+
+  // Handles routing based on hashroutes
+  locationHandler = async () => {
+    let location = window.location.hash.replace('#', '');
+
+    // find page routes
+    let route = location.length == 0 ? this.home : this.routes.find(item => item.slug === location) || this.notFound;
+
+    // update based on route
+    document.title = route.title;
+    this.markup = route.markup;
+  };
+
+  @Watch('routes')
+  routesHandler() {
+    this.locationHandler();
+  }
+
+  componentDidLoad() {
+    // Send event
+    this.abgRouter.emit();
+    // listen for hashevents and output to console
+    window.addEventListener('hashchange', () => {
+      this.locationHandler();
+    });
+  }
+
+  render() {
+    return <Host innerHTML={this.markup}></Host>;
+  }
+}
